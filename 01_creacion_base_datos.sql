6eae874d18cc7b151767be6309b1601b8e2452d6
oriana02 <or.solorzano@duocuc.cl>
2026-05-14 13:16:42 -0400

    se completo docker-compose

--- 01_creacion_base_datos.sql
+++ 01_creacion_base_datos.sql
@@ -11,6 +11,9 @@
 -- DROP DATABASE IF EXISTS proyecto_db;
 
 -- Crear base de datos principal
+SET NAMES utf8mb4;
+SET CHARACTER SET utf8mb4;
+
 CREATE DATABASE IF NOT EXISTS proyecto_db 
 CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
